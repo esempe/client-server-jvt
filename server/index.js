2f14efec4825b7a32e279f9de0194497c03908a8
esempe <steam.dota.return@gmail.com>
2022-11-28 21:12:29 +0200

    server: added router, added blank user-controller

--- server/index.js
+++ server/index.js
@@ -3,13 +3,16 @@ const express = require('express');
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const mongoose = require('mongoose')
+const router = require("./router/index")
 
 const PORT =  process.env.PORT || 5000
 const app = express()
 
 app.use(express.json())
+app.use('/api', router)
 app.use(cookieParser())
 app.use(cors)
+
 
 const start = async () => {
     try {
---
+++ server/models/user-model.js
@@ -0,0 +1,10 @@
+const {Schema, model} = require('mongoose')
+
+const UserSchema = new Schema({
+    email: {type:String, unique:true, required:true},
+    password: {type:String, required:true},
+    isActivated: {type:String,required:true},
+    activationLink: {type:String,required:true},
+})
+
+module.exports = model('User',UserSchema)
